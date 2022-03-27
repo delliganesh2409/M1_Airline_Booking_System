d3f6f4f6160ef399997f6952ac26daf1d7d5a27c
delliganesh2409 <101784923+delliganesh2409@users.noreply.github.com>
2022-03-27 12:15:19 +0530

    Commit #27

---
+++ 3_Implementation/message_fun.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<string.h>
+#include <ctype.h>
+void headMessage(const char *message)
+{
+    system("cls");
+    printf("\n\t\t\t***************************************************************************");
+    printf("\n\t\t\t************                                                   ************");
+    printf("\n\t\t\t************           Airline Booking System                  ************");
+    printf("\n\t\t\t************                                                   ************");
+    printf("\n\t\t\t***************************************************************************");
+    printMessageCenter(message);
+}
+void printMessageCenter(const char* message)
+{
+    int len =0;
+    int pos = 0;
+    len = (78 - strlen(message))/2;
+    printf("\t\t\t");
+    for(pos =0 ; pos < len ; pos++)
+    {
+        printf(" ");
+    }
+    printf("%s",message);
+}./
+void welcomeMessage()
+{
+    printf("\n\n\n\n\n");
+    printf("\n\t\t\t  **-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**\n");
+    printf("\n\t\t\t        =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+    printf("\n\t\t\t        =                  WELCOME                  =");
+    printf("\n\t\t\t        =                    TO                     =");
+    printf("\n\t\t\t        =              AIRLINE BOOKING              =");
+    printf("\n\t\t\t        =                   SYSTEM                  =");
+    printf("\n\t\t\t        =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+    printf("\n\t\t\t  **-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**\n");
+    printf("\n\n\n\t\t\t Enter any key to continue.....");
+    getchar();
+}
